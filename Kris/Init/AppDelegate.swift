800a84b1c9e637790513eec1c6134cba980f25d4
Kris Jackson <kristopherrjackson@gmail.com>
2020-04-11 14:02:40 -0500

    Push notifications enabled

--- Kris/Init/AppDelegate.swift
+++ Kris/Init/AppDelegate.swift
@@ -14,6 +14,7 @@ import GooglePlaces
 import GoogleMobileAds
 import DeviceCheck
 import SDWebImageLinkPlugin
+import UserNotifications
 
 
 @UIApplicationMain
--- Kris/Tabbar/GoogleAds.swift
+++ Kris/Tabbar/GoogleAds.swift
@@ -21,7 +21,7 @@ extension TabBarViewController: GADBannerViewDelegate {
             let ad = GADBannerView(adSize: kGADAdSizeBanner)
             ad.load(GADRequest())
             ad.delegate = self
-            ad.adUnitID = testID
+            ad.adUnitID = productionID
             ad.rootViewController = self
             ad.backgroundColor = .clear
             ad.translatesAutoresizingMaskIntoConstraints = false
